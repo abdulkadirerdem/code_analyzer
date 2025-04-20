b6d3238c0ddfab223da4891042cc1b9ee2641f08
Abdulkadir <abdulkadir.erdem@newexsecurity.com>
2025-04-20 19:44:39 +0300

    feat: test_scanner.py to print found files

--- tests/test_scanner.py
+++ tests/test_scanner.py
@@ -3,4 +3,5 @@ from core.scanner import find_python_files
 
 def test_find_python_files():
     result = find_python_files("tests/mock_project")
+    print("Dosyalar:", result)
     assert any("a.py" in f for f in result)
